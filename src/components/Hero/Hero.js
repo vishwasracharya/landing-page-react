0c6ae0459fc598e2e9a0983f470b1bbd4b7435ad
Vishwas Acharya <vishwasracharya@gmail.com>
2022-05-19 22:04:17 +0530

    Bugs Fixed

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,9 @@ import './Hero.css'
 const Hero = () => {
     return (
         <section className=''>
-            <img src="img/graphics/hero-graphic-1.png" className='img-fluid position-absolute graphics graphics-1' alt="Graphic" />
-            <img src="img/graphics/hero-graphic-2.png" className='img-fluid position-absolute graphics graphics-2' alt="Graphic" />
-            <img src="img/graphics/hero-graphic-3.png" className='img-fluid position-absolute graphics graphics-3' alt="Graphic" />
+            <img src="img/graphics/hero-graphic-1.png" className='img-fluid position-absolute graphics-1' alt="Graphic" />
+            <img src="img/graphics/hero-graphic-2.png" className='img-fluid position-absolute graphics-2' alt="Graphic" />
+            <img src="img/graphics/hero-graphic-3.png" className='img-fluid position-absolute graphics-3' alt="Graphic" />
             <div className="container">
                 <div className="row m-0 p-0 vertical-center d-flex align-item-center justify-content-center">
                     <div className="col-12 col-md-7">
@@ -24,5 +24,5 @@ const Hero = () => {
         </section>
     )
 }
-
+// "homepage": "https://vishwasracharya.github.io/landing-page-react",
 export default Hero;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,12 +6,14 @@ const Navbar = () => {
     return (
         <div>
             <div className="">
-                <img src="img/graphics/nav-graphic-1.png" className='img-fluid position-absolute graphics' style={{left: 0}} alt="Graphic" />
-                <img src="img/graphics/nav-graphic-2.png" className='img-fluid position-absolute graphics' style={{right: 0}} alt="Graphic" />
+                <img src="img/graphics/nav-graphic-1.png" className='img-fluid position-absolute graphics' style={{ left: 0 }} alt="Graphic" />
+                <img src="img/graphics/nav-graphic-2.png" className='img-fluid position-absolute graphics' style={{ right: 0 }} alt="Graphic" />
                 <nav className="navbar navbar-expand-lg navbar-light">
                     <div className="container">
                         <div>
-                            <img src="logo.png" className='img-fluid logo-img' alt="Logo" />
+                            <a href="/">
+                                <img src="logo.png" className='img-fluid logo-img' alt="Logo" />
+                            </a>
                         </div>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
